b3900c54330d527eeaa6c502d2de9cc74f879a9e
Beii <81559438+Jacky72@users.noreply.github.com>
2025-08-22 11:05:03 +0800

    Config the routing between pages

--- priority-const/src/app/page.js
+++ priority-const/src/app/page.js
@@ -1,11 +1,9 @@
 import '@/app/globals.css';
-import Navbar from '@/app/components/navbar/NavBar';
 import Homepage from '@/app/pages/home';
 
 export default function Home({ Component, pageProps }) {
   return (
     <main className="flex min-h-screen flex-col bg-[#141312]" > 
-      <Navbar />
       <Homepage />
     </main>
   );
